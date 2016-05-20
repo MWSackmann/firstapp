d6839d3e9fc1af5fcbc3d83bbdff9425e8b7abd2
Michael Sackmann <michael.sackmann@sap.com>
2016-05-20 10:41:56 +0200

    improve logger messages

--- src/main/java/com/example/Controllers/PostController.java
+++ src/main/java/com/example/Controllers/PostController.java
@@ -32,7 +32,7 @@ public class PostController {
     @RequestMapping(value = "", method = RequestMethod.GET, produces = {"application/json"})
     @ResponseBody
     public ResponseEntity get() {
-        LOGGER.info("get Method called");
+        LOGGER.info("METHOD CALLED: get");
         return ResponseEntity.ok((List<Post>) repository.findAll());
     }
 
@@ -40,7 +40,7 @@ public class PostController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = {"application/json"})
     @ResponseBody
     public ResponseEntity getById(@PathVariable("id") long id) {
-        LOGGER.info("getById Method called with id "+id);
+        LOGGER.info("METHOD CALLED: getById with id " + id);
         final Post readPost = repository.findOne(id);
         if (readPost == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -51,8 +51,8 @@ public class PostController {
     // method creates new post
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity post(@RequestBody Post post) {
-        LOGGER.info("post Method called");
         repository.save(post);
+        LOGGER.info("METHOD CALLED: post with id " + post.getId());
         String response = String.format("id: %s", post.getId());
         return new ResponseEntity<String>(response, HttpStatus.CREATED);
     }
@@ -60,7 +60,7 @@ public class PostController {
     // method updates a single post via its id
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
     public ResponseEntity put(@PathVariable("id") long id, @RequestBody Post post) {
-        LOGGER.info("put Method called with id "+id);
+        LOGGER.info("METHOD CALLED: put with id " + id);
         final Post readPost = repository.findOne(id);
         if (readPost == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -75,7 +75,7 @@ public class PostController {
     // method deletes single post via its id
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity deleteById(@PathVariable("id") long id) {
-        LOGGER.info("deleteById Method called with id "+id);
+        LOGGER.info("METHOD CALLED: deleteById with id " + id);
         final Post readPost = repository.findOne(id);
         if (readPost == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -91,7 +91,7 @@ public class PostController {
     // method selects all posts, loads list.hmtl
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String listPost(Model model) {
-        LOGGER.info("UI list Method called");
+        LOGGER.info("UI METHOD CALLED: list");
         model.addAttribute("posts", repository.findAll());
         return "posts/list";
     }
@@ -99,7 +99,7 @@ public class PostController {
     // method deletes single post via id, via redirect reloads list view again
     @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
     public ModelAndView delete(@PathVariable long id) {
-        LOGGER.info("UI delete Button executed with id "+id);
+        LOGGER.info("UI METHOD CALLED: delete with id " + id);
         repository.delete(id);
         return new ModelAndView("redirect:/posts/list");
     }
@@ -107,22 +107,23 @@ public class PostController {
     // method loads view for new post
     @RequestMapping(value = "/new", method = RequestMethod.GET)
     public String newProject() {
-        LOGGER.info("create Button executed");
+        LOGGER.info("UI METHOD CALLED: new");
         return "posts/new";
     }
 
     // method is triggered via submit button on create.html, does save, reloads list.hmtl
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ModelAndView create(@RequestParam("message") String comment) {
-        LOGGER.info("UI new post created");
-        repository.save(new Post(comment));
+        Post post = new Post(comment);
+        repository.save(post);
+        LOGGER.info("UI METHOD CALLED: new post saved with id " + post.getId());
         return new ModelAndView("redirect:/posts/list");
     }
 
     // method loads view for editing existing post
     @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
     public String edit(@PathVariable long id, Model model) {
-        LOGGER.info("edit Button executed");
+        LOGGER.info("UI METHOD CALLED: edit with id " + id);
         Post post = repository.findOne(id);
         model.addAttribute("post", post);
         return "posts/edit";
@@ -132,7 +133,7 @@ public class PostController {
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ModelAndView update(@RequestParam("post_id") long id,
                                @RequestParam("message") String message) {
-        LOGGER.info("UI post with id "+id+" changed");
+        LOGGER.info("UI METHOD CALLED: update with id " + id);
         Post post = repository.findOne(id);
         post.setMessage(message);
         repository.save(post);
