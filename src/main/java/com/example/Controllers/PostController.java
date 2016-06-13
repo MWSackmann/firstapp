afd83c93cb735ea8561f7bfa0b0e44dba4f7e240
Michael Sackmann <michael.sackmann@sap.com>
2016-06-13 16:26:09 +0200

    refactor logging messages

--- src/main/java/com/example/Controllers/PostController.java
+++ src/main/java/com/example/Controllers/PostController.java
@@ -42,7 +42,7 @@ public class PostController {
     // method returns single post via its id (key)
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = {"application/json"})
     public ResponseEntity getById(@PathVariable("id") long id) {
-        LOGGER.info("METHOD CALLED: getById with id " + id);
+        LOGGER.info("METHOD CALLED: getById with id {}", id);
         final Post readPost = repository.findOne(id);
         if (readPost == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -57,7 +57,7 @@ public class PostController {
             return new ResponseEntity<String>("Defining an ID is not allowed in POST request", HttpStatus.BAD_REQUEST);
         }
         repository.save(post);
-        LOGGER.info("METHOD CALLED: post with id " + post.getId());
+        LOGGER.info("METHOD CALLED: post with id {}", post.getId());
 
         // we do not return the created entity in body, instead we return the URL to the created entity in response header
         final URI uriOfCreatedPost = ServletUriComponentsBuilder.fromCurrentContextPath().path(String.format("posts/%s", post.getId())).build().toUri();
@@ -69,7 +69,7 @@ public class PostController {
     // method updates a single post via its id
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
     public ResponseEntity put(@PathVariable("id") long id, @RequestBody Post post) {
-        LOGGER.info("METHOD CALLED: put with id " + id);
+        LOGGER.info("METHOD CALLED: put with id {}", id);
         final Post readPost = repository.findOne(id);
         if (readPost == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -86,7 +86,7 @@ public class PostController {
     // method deletes single post via its id
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity deleteById(@PathVariable("id") long id) {
-        LOGGER.info("METHOD CALLED: deleteById with id " + id);
+        LOGGER.info("METHOD CALLED: deleteById with id {}", id);
         final Post readPost = repository.findOne(id);
         if (readPost == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -110,7 +110,7 @@ public class PostController {
     // method deletes single post via id, via redirect reloads list view again
     @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
     public ModelAndView delete(@PathVariable long id) {
-        LOGGER.info("UI METHOD CALLED: delete with id " + id);
+        LOGGER.info("UI METHOD CALLED: delete with id {}", id);
         repository.delete(id);
         return new ModelAndView("redirect:/posts/list");
     }
@@ -127,14 +127,14 @@ public class PostController {
     public ModelAndView create(@RequestParam("message") String comment) {
         Post post = new Post(comment);
         repository.save(post);
-        LOGGER.info("UI METHOD CALLED: new post saved with id " + post.getId());
+        LOGGER.info("UI METHOD CALLED: new post saved with id {}", post.getId());
         return new ModelAndView("redirect:/posts/list");
     }
 
     // method loads view for editing existing post
     @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
     public String edit(@PathVariable long id, Model model) {
-        LOGGER.info("UI METHOD CALLED: edit with id " + id);
+        LOGGER.info("UI METHOD CALLED: edit with id {}", id);
         Post post = repository.findOne(id);
         model.addAttribute("post", post);
         return "posts/edit";
@@ -144,7 +144,7 @@ public class PostController {
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ModelAndView update(@RequestParam("post_id") long id,
                                @RequestParam("message") String message) {
-        LOGGER.info("UI METHOD CALLED: update with id " + id);
+        LOGGER.info("UI METHOD CALLED: update with id {}", id);
         Post post = repository.findOne(id);
         post.setMessage(message);
         repository.save(post);
