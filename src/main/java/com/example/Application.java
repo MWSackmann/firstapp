c8d381c93304632d90610bd5f14a5f7213117d23
Michael Sackmann <michael.sackmann@sap.com>
2016-08-04 17:44:08 +0200

    change ORM from hibernate to eclipselink

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,15 +3,37 @@ package com.example;
 // this are  basic settings to make class executable class by springboot
 // works via annotation
 
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends JpaBaseConfiguration {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+        return new EclipseLinkJpaVendorAdapter();
+    }
+
+    @Override
+    protected Map<String, Object> getVendorProperties() {
+        final HashMap<String, Object> map = new HashMap<>();
+        map.put(PersistenceUnitProperties.WEAVING, detectWeavingMode());
+        return map;
+    }
+
+    private String detectWeavingMode() {
+        return InstrumentationLoadTimeWeaver.isInstrumentationAvailable() ? "true" : "static";
+    }
 }
--- src/main/java/com/example/repository/PostRepository.java
+++ src/main/java/com/example/repository/PostRepository.java
@@ -1,17 +1,24 @@
 package com.example.repository;
 
 import com.example.model.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 /**
  * Created by sackmann on 02.05.2016.
  */
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
     // default sort by primary key
     List<Post> findAllByOrderByIdAsc();
 
     // newest post on top
     List<Post> findAllByOrderByCreatedAtDesc();
 }
+
+//public interface PostRepository extends RevisionRepository<Post, Long, Integer>, CrudRepository<Post, Long> {
+//    // default sort by primary key
+//    List<Post> findAllByOrderByIdAsc();
+//    // newest post on top
+//    List<Post> findAllByOrderByCreatedAtDesc();
+//}
