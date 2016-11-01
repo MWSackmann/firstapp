8e53781f10e5b1c01b24ae21b5b09b6b2b3d949c
MWSackmann <MWSackmann@gmx.de>
2016-11-01 10:21:00 +0100

    upgrade spring boot 141

--- src/test/java/com/example/ApplicationTest.java
+++ src/test/java/com/example/ApplicationTest.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -10,7 +10,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
  * Created by d020447 on 17.05.2016.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest(classes = Application.class)
 @WebAppConfiguration
 public class ApplicationTest {
     @org.junit.Before
